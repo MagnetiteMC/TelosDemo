bf3a6eebaf482099ea980c59924a3b18f3fa6949
MagnetiteMC <35513713+MagnetiteMC@users.noreply.github.com>
2025-10-07 15:01:45 -0400

    Initial commit

---
+++ src/main/kotlin/dev/magnetite/telosdemo/TelosDemo.kt
@@ -0,0 +1,14 @@
+package dev.magnetite.telosdemo
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TelosDemo : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
